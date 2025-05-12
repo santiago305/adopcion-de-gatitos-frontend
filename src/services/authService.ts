5f9621354c20b77d966f64942bb8b249623ce5dd
santiago305 <minecratf633@gmail.com>
2025-05-12 16:30:37 -0500

    feat: add TypeScript typings to loginUser and registerUser functions in authService

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,14 +2,32 @@ import axiosInstance from "@/common/utils/axios";
 import { API_AUTH_GROUP } from "./APIs"
 import { LoginCredentials, RegisterCredentials } from "@/types/auth";
 
-export const loginUser = async (payload: LoginCredentials) => {
-  const response = await axiosInstance.post(API_AUTH_GROUP.authentication, payload);
-  return response.data;
+// Vamos a tipar los servicios ed autenticacion
+interface AuthService {
+  access_token: string;
+  refresh_token?: string;
+  role: string;
+  [key: string]: any;
+}
+
+export const loginUser = async (payload: LoginCredentials):Promise<AuthService> => {
+  try {
+    const response = await axiosInstance.post(API_AUTH_GROUP.authentication, payload);
+    return response.data;
+  } catch (error) {
+    console.error("error en loginUser",error);
+    throw error;
+  }
 };
 
-export const registerUser = async (payload: RegisterCredentials) => {
-  const response = await axiosInstance.post(API_AUTH_GROUP.register, payload)
-  return response.data;
+export const registerUser = async (payload: RegisterCredentials):Promise<AuthService> => {
+  try {
+    const response = await axiosInstance.post(API_AUTH_GROUP.register, payload)
+    return response.data;
+  } catch (error) {
+    console.error("error en registerUser",error);
+    throw error;
+  }
 }
 
 export const checkTokenValidity = async () => {
