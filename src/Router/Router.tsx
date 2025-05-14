12add300c9a8da19259158f4391cfdc3d063d0fa
santiago305 <minecratf633@gmail.com>
2025-05-14 13:51:54 -0500

    feat: simplify router configuration by removing dashboard route and integrating dashboardRoutes for improved organization

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,33 +1,16 @@
-// routes/router.ts
-import { Dashboard } from "@/pages";
 import ErrorPage from "@/pages/Error404";
 import { createBrowserRouter } from "react-router-dom";
-import PrivateRoute from "./guards/PrivateRoute";
-import DashboardLayout from "@/pages/dashboard/DashboardLayout";
 import { publicRoutes } from "./modules/PublicRoutes";
 import { clientsRoutes } from "./modules/ClientRoutes";
 import { authRoutes } from "./modules/AuthRoutes";
+import { dashboardRoutes } from "./modules/DashboardRoutes";
 
 
 export const router = createBrowserRouter([
   ...publicRoutes,
   ...authRoutes,
   ...clientsRoutes,
-  {
-    path: "/dashboard",
-    element: (
-      <PrivateRoute>
-        <DashboardLayout />
-      </PrivateRoute>
-    ),
-    errorElement: <ErrorPage />,
-    children: [
-      { index: true, element: <Dashboard /> }, // /dashboard
-      // { path: "settings", element: <Settings /> }, // /dashboard/settings
-      // { path: "profile", element: <Profile /> }  // /dashboard/profile
-    ]
-  },
-  
+  ...dashboardRoutes,
   {
     path: "*",
     element: <ErrorPage />,
