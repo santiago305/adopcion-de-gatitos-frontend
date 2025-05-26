5b18208bc52fb75e8eba05fd73d4986a71dfcea8
santiago305 <minecratf633@gmail.com>
2025-05-26 18:04:40 -0500

    Refactor code structure for improved readability and maintainability

--- src/pages/public/Home.tsx
+++ src/pages/public/Home.tsx
@@ -6,7 +6,12 @@ export default function Home() {
   useLocationFlashMessage();
     return (
       <>
-      hola
+        <div
+        className="w-full h-screen bg-cover bg-center bg-amber-300"
+        style={{ backgroundImage: "url('/assets/fondohome.webp')" }}
+        >
+
+        </div>
       </> 
     );
 }
