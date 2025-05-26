7abd562f45b125bed0c44040fb1672bb7887e4ee
santiago305 <minecratf633@gmail.com>
2025-05-26 17:52:33 -0500

    feat: add framer-motion for header animations and enhance visibility effects

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,6 +5,7 @@ import HeaderNav from "./HeaderNav";
 import HeaderUserAction from "./HeaderUserAction";
 import MobileMenuButton from "./MobileMenuButton";
 import MobileMenu from "./MobileMenu";
+import { motion } from 'framer-motion';
 
 export default function Header() {
   const [showHeader, setShowHeader] = useState(true);
@@ -37,23 +38,36 @@ export default function Header() {
           showHeader ? "translate-y-0" : "-translate-y-full"
         )}
       >
-        <section
+        <motion.section
           className="w-full h-full max-w-[1200px] m-auto flex bg-white/80 
                      backdrop-blur-md rounded-4xl px-10"
+          initial={{ opacity: 0, }}
+          animate={{ opacity: 1, }}
+          transition={{ duration: 0.5 }}
         >
           <div className="w-full h-full flex items-center justify-between">
             <div className="md:hidden">
               <MobileMenuButton isOpen={menuOpen} toggle={toggleMenu} />
             </div>
+            <motion.div
+            initial={{ opacity: 0, x: -30 }}  
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5 }}
+            >
+              <Logo className="w-[80px]" />
+            </motion.div>
 
-            <Logo className="w-[80px]" />
-
-            <div className="flex gap-10">
+            <motion.div 
+            className="flex gap-10"
+            initial={{ opacity: 0, x: 50 }}  
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5 }}
+            >
               <HeaderNav />
               <HeaderUserAction />
-            </div>
+            </motion.div>
           </div>
-        </section>
+        </motion.section>
       </div>
       <MobileMenu isOpen={menuOpen} close={closeMenu} />
     </>
